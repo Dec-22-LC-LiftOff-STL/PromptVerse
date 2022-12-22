9c0f6880f0bae809ab72d176a6fe3f82d71bc928
S1bettig <s1bettig@gmail.com>
2022-12-21 19:29:58 -0600

    Part one of mob

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import './index.css';
 import CreateAccount from './components/User/CreateAccount';
 import Header from './components/Ui/Header';
-
+import UserLogin from './components/User/UserLogin.js';
 
 function App() {
   return (
     <div className="flex flex-col w-full h-screen gap-5 items-center bg-slate-600">
       <Header />
-      <CreateAccount />
+      {/* <CreateAccount /> */}
+      <UserLogin />
     </div>
   );
 }
