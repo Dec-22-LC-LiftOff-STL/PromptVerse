0403a3407bee0ec71029eb07953339146451b7f9
jediknight813 <connor813@outlook.com>
2023-01-05 15:41:53 -0600

    started fixing homepage post grid

--- client/src/components/Ui/HomePage.js
+++ client/src/components/Ui/HomePage.js
@@ -1,6 +1,7 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, onr} from "react";
 import { useCookies, Cookies } from 'react-cookie';
 import { getPosts } from "../../actions/PostActions";
+import StackGrid, {transitions} from "react-stack-grid";
 
 
 const Homepage = () => {
@@ -8,41 +9,46 @@ const Homepage = () => {
     const token = cookies.get('user_token');
     const userData = cookies.get('user_data');
     const [posts, setPosts] = useState([]);
-
+    const { scaleDown } = transitions;
     const [update, setUpdate] = useState(false)
-  
+    
 
     const handleSubmit = async (event) => {
         const data = await getPosts()
         console.log(posts)
-        setPosts(data)
+        if (data !== []) {
+            setPosts(data)
+        }
     }
-    
 
+    const [stackGrid, setStackGrid] = useState();
+    
 
     useEffect(() => {
        handleSubmit()
+       if (stackGrid) {
+            stackGrid.updateLayout()
+        }
     }, [update]);
   
 
     return (
-      <>
-        <div className=" w-full md:w-[98%] h-screen flex flex-col gap-2 flex-wrap content-start justify-start">
-
-            {posts !== undefined && 
-            <>
-                {posts.map(post => (
-
-
-                    <div className="h-max flex-grow-1">
-                        <img className=" w-[256px] h-full rounded-md" alt="post_image" src={post.image}/>
-                    </div>
-                ))}
-            </>
-            }
-        </div>
-        {/* {hasMore && <button onClick={() => setPage(page + 1)}>Load More</button>} */}
-      </>
+        <StackGrid
+        className=" w-full h-auto"
+        appear={scaleDown.appear}
+        appeared={scaleDown.appeared}
+        enter={scaleDown.enter}
+        entered={scaleDown.entered}
+        leaved={scaleDown.leaved}
+        columnWidth={210}
+        gridRef={grid => setStackGrid(grid)}
+        monitorImagesLoaded={true}>
+            {posts.map(post => (
+                <div key={post._id} className="h-max flex-grow-1 cursor-pointer hover:opacity-80">
+                    <img className=" w-[256px] h-full rounded-md" alt="post_image" src={post.image}/>
+                </div>
+            ))}
+        </StackGrid>
     );
 
 }
